5b0beff0ad35a643e1cbc1beba2c66e4a8691e8e
thanglong0802 <thanglong08022k@gmail.com>
2023-03-23 17:11:49 +0700

    test api

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./css/style.css";
+import axios from "axios";
 
 function Category() {
+  const [api, setApi] = useState([]);
+  useEffect(() => {
+    const fetchItem = async () => {
+      try {
+        let rs = await axios.get("https://jsonplaceholder.typicode.com/posts");
+        setApi(rs.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchItem();
+  }, []);
+  console.log(api);
   return (
     <div className="category">
       <ul className="nav-ul">
-        {/* <li>Category</li> */}
-        Category
+        {api.map((ele) => (
+          <li>{ele.title}</li>
+        ))}
       </ul>
     </div>
   );
--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,8 +1,46 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./css/style.css";
 
 function Content() {
-  return <div className="content">Content</div>;
+  const [img, setImg] = useState([]);
+  useEffect(() => {
+    const fetchImg = async () => {
+      try {
+        let rs = await axios.get(
+          "https://jsonplaceholder.typicode.com/albums/1/photos"
+        );
+        setImg(rs.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchImg();
+  }, []);
+  return (
+    <div className="content">
+      <div className="products">
+        <h3>Sản phầm nổi bật</h3>
+        <div className="product-list row row-cols-lg-3 row-cols-md-2 row-cols-1">
+          <div className="product-item">
+            {img.map((ele) => (
+              <div className="product-item-inner card">
+                <a href="#product">
+                  <img src={ele.url} alt="" />
+                </a>
+                <h4>
+                  <a href="$">iPhone Xs Max 2 Sim</a>
+                </h4>
+                <p>
+                  giá bán: <span>32.990.000đ</span>
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Content;
